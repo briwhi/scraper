dadd0506c73e2cb265ab351d24d80e6e1c62481b
Brian Whiteley <brianwhiteleybw@gmail.com>
2020-11-20 18:53:32 -0600

    start icloud

--- scraper.py
+++ scraper.py
@@ -1,2 +1,15 @@
 #scraper.py
-:
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from config import User
+import time
+
+driver = webdriver.Firefox()
+driver.get("http://icloud.com")
+user = User()
+username = user.username
+password = user.password
+time.sleep(10)
+element = driver.find_element_by_id('account_name_text_field')
+element.send_keys(username)
